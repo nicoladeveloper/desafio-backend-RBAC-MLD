e89aee7c9818e9e71a81ca352d231dac97c412f1
Nicolas Oliveira <ffnicolaf13@gmail.com>
2026-02-02 19:57:35 -0300

    Add RolesController to UsersModule

--- src/Funcionarios/Funcionario.module.ts
+++ src/Funcionarios/Funcionario.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { UsersService } from './Funcionario.service';
 import { UsersController } from './Funcionario.controller';
+import { RolesController } from './roles.controller';
+import { PrismaModule } from '../prisma/prisma.module';
 
 @Module({
-  controllers: [UsersController],
+  imports: [PrismaModule],
+  controllers: [UsersController, RolesController],
   providers: [UsersService],
   exports: [UsersService],
 })
