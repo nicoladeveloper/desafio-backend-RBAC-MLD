89f64db1504f830ffa3cdaf0e32d4011c836fd16
Nicolas Oliveira <ffnicolaf13@gmail.com>
2026-02-02 15:09:37 -0300

    Refactor comments and improve code clarity in auth service

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -11,15 +11,15 @@ export class AuthService {
   ) {}
 
   async login(email: string, senhaPlana: string) {
-    // 1. Busca o funcionário pelo email e traz os dados da Role
+    // Busca o funcionário pelo email e traz os dados da Role
     const user = await this.prisma.funcionario.findUnique({
       where: { email },
       include: {
-        role: true, // Simplificado para pegar os dados da Role
+        role: true,
       },
     });
 
-    // 2. Valida existência e senha
+    //Validando existência e senha
     if (!user) {
       throw new UnauthorizedException('E-mail ou senha incorretos');
     }
@@ -30,12 +30,11 @@ export class AuthService {
       throw new UnauthorizedException('E-mail ou senha incorretos');
     }
 
-    // 3. CORREÇÃO DO PAYLOAD: 
-    // Usamos 'nome' em vez de 'name' para bater com o banco de dados
+  
     const payload = {
       sub: user.id,
       email: user.email,
-      role: user.role.nome, // <--- Aqui estava o erro!
+      role: user.role.nome,
     };
 
     return {
